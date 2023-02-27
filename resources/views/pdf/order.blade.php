13ef846829617144b6059db968c75bae5cf18ac2
Bosko Boskovic <bosko.boskovic@gmail.com>
2023-02-27 12:56:34 +0100

    Add more columns to pdf

--- resources/views/pdf/order.blade.php
+++ resources/views/pdf/order.blade.php
@@ -35,41 +35,48 @@
                 <tbody>
                     <tr>
                         <td class="fw-bold w-25">Broj porudžbenice</td>
-                        <td>{{ $order->order_number }}</td>
+                        <td>{{ $order?->order_number }}</td>
                     </tr>
                     <tr>
                         <td class="fw-bold w-25">Način dostave</td>
-                        <td>{{ $order->delivery_type }}</td>
+                        <td>{{ $order?->delivery_type }}</td>
                     </tr>
                     <tr>
                         <td class="fw-bold w-25">Datum poručivanja</td>
-                        <td>{{ $order->order_date }}</td>
+                        <td>{{ $order?->order_date }}</td>
                     </tr>
                     <tr>
                         <td class="fw-bold w-25">Datum isporuke</td>
-                        <td>{{ $order->delivery_date }}</td>
+                        <td>{{ $order?->delivery_date }}</td>
                     </tr>
                     <tr>
                         <td class="fw-bold w-25">Napomena</td>
-                        <td>{{ $order->napomena }}</td>
+                        <td>{{ $order?->napomena }}</td>
                     </tr>
                     <tr>
                         <td class="fw-bold w-25">Poručeno sa</td>
-                        <td>{{ $order->order_from }}</td>
+                        <td>{{ $order?->order_from }}</td>
                     </tr>
                     <tr>
                         <td class="fw-bold w-25">Status</td>
-                        <td>{{ $order->status->status }}</td>
+                        <td>{{ $order?->status->status }}</td>
                     </tr>
+
+                    @if ($order->price)
+                        <tr>
+                            <td class="fw-bold w-25">Suma</td>
+                            <td>{{ number_format($order->price, 2) }} RSD.</td>
+                        </tr>
+                    @endif
 
                     <tr>
                         <td class="fw-bold w-25">Poručio</td>
-                        <td>{{ $order->customer->name }}</td>
+                        <td>{{ $order?->customer?->name }}</td>
                     </tr>
 
                     <tr>
                         <td class="fw-bold w-25">Broj telefona</td>
-                        <td>{{ $order->customer->phone }}</td>
+                        <td>{{ $order?->customer?->phone }}</td>
                     </tr>
 
                     @if ($order->customer->street)
@@ -89,105 +96,41 @@
             </table>
         </div>
 
-
-
-
-        <div class="col-md-10">
-            <h2
-                style="text-align: center; margin: 0 auto; padding: 30px; font-weight: bolder; font-style: 'Helvetica'; ">
-                Proizvodi u porudžbini - {{ $order->order_items->count() }}</h2>
-            <table class="table table-bordered table-striped">
-                <thead>
-                    <tr bgcolor="#48c2ea">
-                        <th>Tip proizvoda</th>
-                        <th>Veličina bedža/majice</th>
-                        <th>
-                            Tip kačenja/ Boja majice
-                        </th>
-                        <th>
-                            Plastifikacija/Tip majice
-                        </th>
-                        <th>Količina</th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                    @foreach ($order->order_items as $item)
-                        <tr>
-                            <th>{{ ucfirst($item->type) }}</th>
-                            <th>{{ $item->badge_size ?? $item->tshirt_size }}</th>
-                            <td>{{ $item->tip_kacenja ?? $item->tshirt_color }}</td>
-                            <td>{{ ucfirst($item->plastifikacija ?? $item->tshirt_type) }}</td>
-                            <td>{{ $item->quantity }}
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-
-
-        {{-- 
-        @if ($buy_bills->count())
-            <div class="col-md-12">
-                <h3
+        @if ($order->order_items->count() > 0)
+            <div class="col-md-10">
+                <h2
                     style="text-align: center; margin: 0 auto; padding: 30px; font-weight: bolder; font-style: 'Helvetica'; ">
-                    Moji računi nabavke {{ $date }} - suma: {{ $sum_bills_buys }} RSD.
-                    ({{ $buy_bills->count() }})</h3>
+                    Proizvodi u porudžbini - {{ $order->order_items->count() }}</h2>
                 <table class="table table-bordered table-striped">
                     <thead>
-                        <tr bgcolor="#fdb817">
-                            <th scope="col">Nabavka</th>
-                            <th scope="col">Šifra</th>
-                            <th scope="col">Iznos</th>
-                            <th scope="col">Datum računa</th>
+                        <tr bgcolor="#48c2ea">
+                            <th>Tip proizvoda</th>
+                            <th>Veličina bedža/majice</th>
+                            <th>
+                                Tip kačenja/ Boja majice
+                            </th>
+                            <th>
+                                Plastifikacija/Tip majice
+                            </th>
+                            <th>Količina</th>
                         </tr>
                     </thead>
                     <tbody>
 
-                        @foreach ($buy_bills->get() as $buy_bill)
+                        @foreach ($order->order_items as $item)
                             <tr>
-                                <th style="width: 700px;">{{ $buy_bill->buy->offer }}</th>
-                                <td>{{ $buy_bill->buy->code }}</td>
-                                <td>{{ number_format($buy_bill->vrednost, 2) }}</td>
-                                <td>{{ date('d.m.Y H:m', strtotime($buy_bill->created_at)) }}</td>
+                                <th>{{ ucfirst($item->type) }}</th>
+                                <th>{{ $item->badge_size ?? $item->tshirt_size }}</th>
+                                <td>{{ $item->tip_kacenja ?? $item->tshirt_color }}</td>
+                                <td>{{ ucfirst($item->plastifikacija ?? $item->tshirt_type) }}</td>
+                                <td>{{ $item->quantity }}
+                                </td>
                             </tr>
                         @endforeach
                     </tbody>
                 </table>
             </div>
         @endif
-
-        @if ($buy_reservations->count())
-            <div class="col-md-12">
-                <h3
-                    style="text-align: center; margin: 0 auto; padding: 30px; font-weight: bolder; font-style: 'Helvetica'; ">
-                    Moje rezervacije nabavke {{ $date }} - suma: {{ $sum_reservations_buys }} RSD.
-                    ({{ $buy_reservations->count() }})</h3>
-                <table class="table table-bordered table-striped">
-                    <thead>
-                        <tr bgcolor="#fdb817">
-                            <th scope="col">Nabavka</th>
-                            <th scope="col">Šifra nabavke</th>
-                            <th scope="col">Rezervacija</th>
-                            <th scope="col">Datum rezervacije</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                        @foreach ($buy_reservations->get() as $buy_reservation)
-                            <tr>
-                                <th style="width: 900px;">{{ $buy_reservation->buy->offer }}</th>
-                                <td>{{ $buy_reservation->buy->code }}</td>
-                                <td>{{ number_format($buy_reservation->reserved, 2) }}</td>
-                                <td>{{ date('d.m.Y H:m', strtotime($buy_reservation->created_at)) }}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-        @endif --}}
-
     </div>
 
 </body>
